79ec4a27fc1a62746063ba177ed2616d9e0c88da
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2014-09-29 00:11:40 +0530

    added json-schema and rest call support

--- themes/theme0/renderers/index.js
+++ themes/theme0/renderers/index.js
@@ -57,7 +57,7 @@ var render = function (theme, data, meta, require) {
                                                 }
                                                 ]
 
-                        }
+                                    }
 
                     }
                 }
