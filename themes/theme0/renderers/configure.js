f206b5da12f6d2d038353326e6d030475967c58f
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2014-10-02 10:44:16 +0530

    added error handling page content

--- themes/theme0/renderers/configure.js
+++ themes/theme0/renderers/configure.js
@@ -97,7 +97,38 @@ var render = function (theme, data, meta, require) {
         });
 
     }else{
+        theme('index', {
+            page_meta: [
+                {
+                    partial:'index_title',
+                    context:{
+                        page_title:'Apache Stratos Home - Error',
+                        page_description:'Apache Stratos Home - Error'
+                    }
+                }
+            ],
+            header:[
+                {
+                    partial: 'index_header',
+                    context:{
+                    }
+                }
+            ],
+            content: [
 
+                {
+                    partial: 'error_page',
+                    context:{
+                        error:data.error,
+                        content_title:'Sorry Something went Wrong...! ',
+                        content_body:{
+
+                        }
+
+                    }
+                }
+            ]
+        });
 
     }
 };
