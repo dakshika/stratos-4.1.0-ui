584853b276e267ce9fe608110043606546977bfa
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2014-10-14 16:54:45 +0530

    my cartridges implementation done

---
+++ themes/theme0/helpers/my_cartridges.js
@@ -0,0 +1,7 @@
+
+var resources = function (page, meta) {
+    return {
+        js: ['custom/form.js' ],
+        css: [ 'custom/style.css']
+    };
+};
---
+++ themes/theme0/renderers/my_cartridges_info.js
@@ -0,0 +1,97 @@
+var render = function (theme, data, meta, require) {
+
+    if(data.error.length == 0 ){
+        theme('index', {
+            page_meta: [
+                {
+                    partial:'index_title',
+                    context:{
+                        page_title:'Apache Stratos My Cartrides',
+                        page_description:'Apache Stratos My Cartrides'
+                    }
+                }
+            ],
+            header:[
+                {
+                    partial: 'index_header',
+                    context:{
+                        user_name:'admin@wso2.com'
+                    }
+                }
+            ],
+            sub_header:[
+                {
+                    partial:'index_sub_header',
+                    context:{
+                        breadcrumbPathLevelOne:data.breadcrumbPathLevelOne,
+                        breadcrumbPathLevelTwo:data.breadcrumbPathLevelTwo
+                    }
+                }
+            ],
+            left_menu:[
+                {
+                    partial:'index_left_menu',
+                    context:{
+                        left_menu:data.left_menu
+                    }
+                }
+            ],
+            right_menu_help:[
+                {
+                    partial:'index_right_menu_help',
+                    context:{
+
+                    }
+                }
+            ],
+            content: [
+
+                {
+                    partial: 'my_cartridges_info',
+                    context:{
+                        content_title:'My Cartridge: ',
+                        content_body:{
+                            myCartridgeInfo: data.myCartridgeInfo
+                        }
+
+                    }
+                }
+            ]
+        });
+
+    }else{
+        theme('index', {
+            page_meta: [
+                {
+                    partial:'index_title',
+                    context:{
+                        page_title:'Apache Stratos Home - Error',
+                        page_description:'Apache Stratos Home - Error'
+                    }
+                }
+            ],
+            header:[
+                {
+                    partial: 'index_header',
+                    context:{
+                    }
+                }
+            ],
+            content: [
+
+                {
+                    partial: 'error_page',
+                    context:{
+                        error:data.error,
+                        content_title:'Sorry Something went Wrong...! ',
+                        content_body:{
+
+                        }
+
+                    }
+                }
+            ]
+        });
+
+    }
+};
