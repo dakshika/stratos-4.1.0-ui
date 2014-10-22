a6ceff638def14dc5b15aba7e58c1ee86ca1fa6c
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2014-10-22 09:27:52 +0530

    wizard based config added to system

--- themes/theme0/helpers/users_form.js
+++ themes/theme0/helpers/users_form.js
@@ -1,6 +1,6 @@
 var resources = function (page, meta) {
     return {
-        js: ['JSONEditor-0.7.10/jsoneditor-0.7.10.js', 'custom/script.js', 'custom/form.js' ],
+        js: ['JSONEditor-0.7.10/jsoneditor-0.7.12.js', 'custom/script.js', 'custom/form.js' ],
         css: [ 'custom/style.css']
     };
 };
---
+++ themes/theme0/helpers/wizard.js
@@ -0,0 +1,6 @@
+var resources = function (page, meta) {
+    return {
+        js: ['wizard/custom.js'],
+        css: ['wizard/custom.css']
+    };
+};
