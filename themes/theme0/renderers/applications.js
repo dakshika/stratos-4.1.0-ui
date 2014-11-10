e619c28af71a86202fd9aed530ad12efb4400971
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2014-11-10 09:26:31 +0530

    add application level context into menu

--- themes/theme0/renderers/applications.js
+++ themes/theme0/renderers/applications.js
@@ -20,78 +20,46 @@
  */
 var render = function (theme, data, meta, require) {
 
-    if (data.error.length == 0) {
-        var log = new Log();
-        session.remove("get-status");
-        session.remove("deploy-status");
-        var cartridges = data.cartridges.cartridge, cartridges_new = [];
-
-
-        for (var i = 0; i < cartridges.length; i++) {
-            if (cartridges[i].serviceGroup != undefined) {
-                if (!cartridges[i].done) {
-
-                    cartridges[i].done = true;
-                    var newObj = {};
-                    var serviceGroup = cartridges[i].serviceGroup;
-                    newObj.serviceGroup = serviceGroup;
-                    newObj.cartridgeType = cartridges[i].cartridgeType;
-                    newObj.items = [];
-                    newObj.items.push(parse(stringify(cartridges[i])));
-                    newObj.version = cartridges[i].version;
-                    for (var j = 0; j < cartridges.length; j++) {
-                        if (cartridges[j].serviceGroup == serviceGroup && !cartridges[j].done) {
-                            cartridges[j].done = true;
-                            newObj.items.push(parse(stringify(cartridges[j])));
-                        }
-                    }
-
-                    cartridges_new.push(newObj);
-                }
-            } else {
-                cartridges_new.push(cartridges[i]);
-            }
-        }
-
+    if(data.error.length == 0 ){
         theme('index', {
             page_meta: [
                 {
-                    partial: 'index_title',
-                    context: {
-                        page_title: 'Apache Stratos - Cartridges',
-                        page_description: 'Apache Stratos - Cartridges'
+                    partial:'index_title',
+                    context:{
+                        page_title:'Apache Stratos - Application Managment',
+                        page_description:'Apache Stratos - Application Managment'
                     }
                 }
             ],
-            header: [
+            header:[
                 {
                     partial: 'index_header',
-                    context: {
-                        user_name: 'admin@wso2.com'
+                    context:{
+                        user_name:'admin@wso2.com'
                     }
                 }
             ],
-            sub_header: [
+            sub_header:[
                 {
-                    partial: 'index_sub_header',
-                    context: {
-                        breadcrumbPathLevelOne: 'applications',
-                        breadcrumbPathLevelTwo: data.breadcrumbPathLevelTwo
+                    partial:'index_sub_header',
+                    context:{
+                        breadcrumbPathLevelOne:'appconfig',
+                        breadcrumbPathLevelTwo:''
                     }
                 }
             ],
-            left_menu: [
+            left_menu:[
                 {
-                    partial: 'index_left_menu',
-                    context: {
-                        left_menu: data.left_menu
+                    partial:'index_left_menu',
+                    context:{
+                        left_menu:data.left_menu
                     }
                 }
             ],
-            right_menu_help: [
+            right_menu_help:[
                 {
-                    partial: 'index_right_menu_help',
-                    context: {
+                    partial:'index_right_menu_help',
+                    context:{
 
                     }
                 }
@@ -99,32 +67,32 @@ var render = function (theme, data, meta, require) {
             content: [
 
                 {
-                    partial: 'applications',
-                    context: {
-                        content_menu: 'links',
-                        content_title: 'Deploy Applications',
-                        content_body: {sections: cartridges_new}
+                    partial: 'metro_menu',
+                    context:{
+                        content_menu:'links',
+                        content_title:'Application Management',
+                        content_body:{sections:data.metro_menu}
 
                     }
                 }
             ]
         });
 
-    } else {
+    }else{
         theme('index', {
             page_meta: [
                 {
-                    partial: 'index_title',
-                    context: {
-                        page_title: 'Apache Stratos Home - Error',
-                        page_description: 'Apache Stratos Home - Error'
+                    partial:'index_title',
+                    context:{
+                        page_title:'Apache Stratos Home - Error',
+                        page_description:'Apache Stratos Home - Error'
                     }
                 }
             ],
-            header: [
+            header:[
                 {
                     partial: 'index_header',
-                    context: {
+                    context:{
                     }
                 }
             ],
@@ -132,10 +100,10 @@ var render = function (theme, data, meta, require) {
 
                 {
                     partial: 'error_page',
-                    context: {
-                        error: data.error,
-                        content_title: 'Sorry Something went Wrong...! ',
-                        content_body: {
+                    context:{
+                        error:data.error,
+                        content_title:'Sorry Something went Wrong...! ',
+                        content_body:{
 
                         }
 
